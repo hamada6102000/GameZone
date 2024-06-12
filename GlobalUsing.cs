b2c304a32a485a06ed8082eef784ee6a54f23173
hamada6102000 <hm2456@fayoum.edu.eg>
2024-06-13 01:01:44 +0300

    Initial commit

---
+++ GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using GameZone.Data;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.Rendering;
+global using GameZone.Models;
+global using GameZone.ViewModels;
+global using GameZone.Services;
+global using GameZone.Settings;
+global using GameZone.Attribute;
+global using GameZone.Attribute;
+
+
